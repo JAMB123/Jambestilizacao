29d9b617de9d046e64cbff89f145e90869361c69
JAMB123 <jamblpa476@gmail.com>
2023-10-05 10:44:31 -0300

    Tudo certo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,82 +6,64 @@
     <div class="col-sm-12" style="background-color: #fffff" style="width: auto; height: 400px">
         <div class="container">
             <p class="h4 text-center" style="color: #2B1998; margin-top: 20px; font-size: 60px"><b>Cadastre-se </b></p>
-            <form method="POST" action="{{ route('register') }}" id="bordaform">
-                @csrf
-                <table id="tabela2">
-                    <tbody>
-                    <tr>
-                <td colspan="2">
-                     <label for="name">Nome:</label>
-                </td>
-                <td colspan="2">
-                    <label for="sobrename">Sobrenome:</label>
-                </td>
-            </tr>
-            <tr>
-                <td><img src="{{asset('storage/imagens/boneco.svg')}}" /></td>
-                <td><input type="text" class="form-control" name="name" id="nome" required></td>
-                <td><img src="{{asset('storage/imagens/boneco.svg')}}" /></td>
-                <td><input type="text" class="form-control" name="sobrename" id="sobrenome" required></td>
+<form method="POST" action="{{ route('register') }}" class="needs-validation" novalidate>
+          <div class="row g-3">
+            <div class="col-sm-6">
+            <img src="{{asset('storage/imagens/boneco.svg')}}" />
+              <label for="firstName" class="form-label">Nome</label>
+              <input type="text" class="form-control" id="firstName" placeholder="Nome" value="" required>
+            </div>
 
-            </tr>
-            <tr>
-                <td colspan="2">
-                     <label for="email">E-mail:</label>
-                </td>
-                <td colspan="2">
-                    <label for="telefone">Telefone/Celular:</label>
-                </td>
-            </tr>
-            <tr>
-                <td><img src="{{asset('storage/imagens/email.svg')}}" /></td>
-                <td><input type="text" class="form-control" name="email" id="nome" required></td>
-                <td><img src="{{asset('storage/imagens/telefone.png')}}" /></td>
-                <td><input type="text" class="form-control" name="telefone" id="nome" required></td>
-
-            </tr>
-            <tr>
-                <td colspan="2">
-                     <label for="password">Senha:</label>
-                </td>
-                <td colspan="2">
-                    <label for="confirm-password">Confirmar senha:</label>
-                </td>
-            </tr>
-            <tr>
-                <td><img src="{{asset('storage/imagens/chaves.png')}}" /></td>
-                <td><input type="password" class="form-control" name="senha" id="nome" required ></td>
-                <td><img src="{{asset('storage/imagens/chaves.png')}}" /></td>
-                <td><input type="password" class="form-control" name="confirm-password" id="nome" required></td>
-
-            </tr>
-            <tr>
-                <td colspan="2">
-                     <label for="CPF">CPF:</label>
-                </td>
-                <td colspan="2">
-                    <label for="username">Nome de Usuário:</label>
-                </td>
-            </tr>
-            <tr>
-                <td><img src="{{asset('storage/imagens/cpf.png')}}" /></td>
-                <td><input type="text" class="form-control"name="CPF" id="nome" required></td>
-                <td><img src="{{asset('storage/imagens/arroba.png')}}" /></td>
-                <td><input type="text" class="form-control" name="username" id="nome" required></td>
-
-            </tr>
-            <tr>
-            <td colspan="2">
-                <td><a href="/">
+            <div class="col-sm-6">
+            <img src="{{asset('storage/imagens/boneco.svg')}}" />
+              <label for="lastName" class="form-label">Sobrenome</label>
+              <input type="text" class="form-control" id="lastName" placeholder="Sobrenome" value="" required>
+            </div>
+            </div>
+            <div class="row g-3">
+            <div class="col-sm-12">
+              <label for="username" class="form-label">Nome de usuário</label>
+              <div class="input-group has-validation">
+                <span class="input-group-text">@</span>
+                <input type="text" class="form-control" id="username" placeholder="Username" required>
+              </div>
+                <br>
+              <div class="col-12">
+              <img src="{{asset('storage/imagens/email.svg')}}" />
+              <label for="email" class="form-label">Email</label>
+              <input type="email" class="form-control" id="email" placeholder="nome@exemplo.com">
+            </div>
+        </div>
             <br>
-            
+              <div class="col-sm-6">
+              <img src="{{asset('storage/imagens/cpf.png')}}" />
+              <label for="firstName" class="form-label">CPF</label>
+              <input type="text" class="form-control" id="firstName" placeholder="999.999.999-99" value="" required>
+            </div>
+            <br>
+            <div class="col-sm-6">
+            <img src="{{asset('storage/imagens/telefone.png')}}" />
+              <label for="lastName" class="form-label">Telefone/Celular</label>
+              <input type="text" class="form-control" id="lastName" placeholder="+99(99)99999-9999" value="" required>
+            </div>
+            <br>
+            <div class="col-6">
+            <img src="{{asset('storage/imagens/chaves.png')}}" />
+              <label for="address" class="form-label">Senha</label>
+              <input type="text" class="form-control" id="address" placeholder="Senha" required>
+            </div>
+            <br>
+            <div class="col-6">
+            <img src="{{asset('storage/imagens/chaves.png')}}" />
+              <label for="address2" class="form-label">Confirmar Senha</label>
+              <input type="text" class="form-control" id="address2" placeholder="Confirmar Senha">
+            </div>
+            <br>
+            <br>
+            <div class="col-6">
             <td><button class="btn btn-primary my-2 my-sm-0"; id="botaocad" type="submit" style="margin-right: 15px">Cadastrar</button></td>
-            <!--<td><p style="color: black" id="titulo"><b>Já tem uma conta? <a style="color: black" href="{{ route('login') }}">Faça Login</a></b></p></td>-->       
-            </tr>
-            </form>
-            </tbody>
-        </table>
-</div>
+            </div>
+
 @endsection
 @section('script')
     <script type="module">
@@ -93,4 +75,5 @@
         });
     </script>
 @endsection
+
 
